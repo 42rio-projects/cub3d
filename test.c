140687e90889a6d0bf79f0c8b40b22e3c51dcbaf
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-02-09 23:55:51 -0300

    fix: put test.c in norminette

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-	char *ola = malloc(10);
+int	main(void)
+{
+	char	*ola;
+
+	ola = malloc(10);
 	ola = "eae";
 	printf("%s\n", ola);
 }
