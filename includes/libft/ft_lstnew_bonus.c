8306b0043de687ecec237881fa793f58ecb4ebbe
Vitor Sergio <vsergio@c2t2s3.42.rio>
2023-03-09 13:33:08 -0300

    fix: starting size value in ft_lstnew

--- includes/libft/ft_lstnew_bonus.c
+++ includes/libft/ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*   By: vsergio <vsergio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 14:44:40 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/18 15:48:59 by vsergio          ###   ########.fr       */
+/*   Updated: 2023/03/09 13:32:54 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ t_list	*ft_lstnew(void *content)
 	if (!lst)
 		return (NULL);
 	lst->content = content;
+	lst->size = 0;
 	lst->next = NULL;
 	return (lst);
 }
