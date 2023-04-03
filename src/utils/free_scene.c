0bc0935921d9c103074fbd696f4f37ebee5200de
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-04-03 18:18:08 -0300

    feat: get map information, check if map is surrounded by wall

--- src/utils/free_scene.c
+++ src/utils/free_scene.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:48:42 by vsergio           #+#    #+#             */
-/*   Updated: 2023/04/03 16:50:02 by vsergio          ###   ########.fr       */
+/*   Updated: 2023/04/03 17:24:34 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	free_scene(t_scene* scene)
 {
-	free_matrix(scene->map);
+	free_matrix(scene->map_grid);
 	free(scene->no_texture.text);
 	free(scene->no_texture.addr);
 	free(scene->so_texture.text);
