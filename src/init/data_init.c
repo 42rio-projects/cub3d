e666c792462640df6f0bf2773ea1d9c732359585
Gabriel Guedes <gguedes@c2t2s1.42.rio>
2023-04-04 17:47:14 -0300

    feat: images init and render tile

--- src/init/data_init.c
+++ src/init/data_init.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 12:12:00 by gguedes           #+#    #+#             */
-/*   Updated: 2023/04/03 18:16:35 by vsergio          ###   ########.fr       */
+/*   Updated: 2023/04/04 17:44:27 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,35 +46,12 @@ bool	data_init(t_data *data, char const *file)
 	file_content = get_file_content(file);
 	if (file_content == NULL)
 		return (1);
-	data->mlx_ptr = mlx_init();
 	set_null_scene(&data->scene);
 	if (elements_init(data, file_content))
-    return (free_matrix(file_content), free_scene(&data->scene), 1);
-  if (map_init(&data->scene, file_content))
-    return (free_matrix(file_content), free_scene(&data->scene), 1);
-	// data->player = player_init(data->map);
-	// if (data->player == NULL) {
-	// 	free_matrix(file_content);
-	// 	free_matrix(data->map->grid);
-	// 	free(data->map);
-	// 	free(data);
-	// 	return 1;
-	// }
-
-	// //ponteiro da janela
-	// data->mlx.win = mlx_new_window(data->mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
-
-	// // create a background image and put to window
-	// data->background_image.img = mlx_new_image(data->mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	// data->background_image.addr = mlx_get_data_addr(data->background_image.img, &data->background_image.bpp, &data->background_image.size_len, &data->background_image.endian);
-	// draw_background(&data->background_image, data->floor_color, data->ceil_color);
-	// mlx_put_image_to_window(data->mlx.ptr, data->mlx.win, data->background_image.img, 0, 0);
-
-	// // creates the main image: player on minimap, rays and walls
-	// data->image.img = mlx_new_image(data->mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	// data->image.addr = mlx_get_data_addr(data->image.img, &data->image.bpp, &data->image.size_len, &data->image.endian);
-	// draw_image(&data->image, data);
-	// mlx_put_image_to_window(data->mlx.ptr, data->mlx.win, data->image.img, 0, 0);
-	// free_matrix(file_content);
+		return (free_matrix(file_content), free_scene(&data->scene), 1);
+	if (map_init(&data->scene, file_content))
+		return (free_matrix(file_content), free_scene(&data->scene), 1);
+	images_init(data);
+	free_matrix(file_content);
 	return (0);
 }
---
+++ src/render/render_tile.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_tile.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 15:49:41 by gguedes           #+#    #+#             */
+/*   Updated: 2023/04/04 16:08:53 by gguedes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	render_tile(t_image *image, uint32_t x_start, uint32_t y_start, uint32_t color)
+{
+	uint32_t	x_end;
+	uint32_t	y_end;
+	uint32_t	x;
+
+	x_end = x_start + MINIMAP_SCALE;
+	y_end = y_start + MINIMAP_SCALE;
+	while (y_start < y_end)
+	{
+		x = x_start;
+		while (x < x_end)
+		{
+			put_pixel(image, x, y_start, color);
+			x++;
+		}
+		y_start++;
+	}
+}
