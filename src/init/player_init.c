0f4fd8074a209b271450be529c8001c171ce950e
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-04-07 15:26:38 -0300

    refactor: change message when have more than 1 player in map

--- src/init/player_init.c
+++ src/init/player_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 12:40:48 by gguedes           #+#    #+#             */
-/*   Updated: 2023/04/06 12:41:26 by gguedes          ###   ########.fr       */
+/*   Updated: 2023/04/07 15:25:37 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static bool	get_player_position(char **map_grid, t_player *player)
 			if (ft_strchr("NSWE", map_grid[y][x]))
 			{
 				if (player->pos_x != 0 || player->pos_y != 0)
-					return (throw_error("Player not found\n"));
+					return (throw_error("You must use only 1 player per map!\n"));
 				player->pos_x = x + 0.5;
 				player->pos_y = y + 0.5;
 			}
