617153fcd6eab29fc8fe1899c46e83731d991ce1
gguedes <gguedes@student.42.rio>
2023-04-08 22:21:31 -0300

    fix: better resposivity

--- src/hooks/mouse_hook.c
+++ src/hooks/mouse_hook.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 20:16:20 by gguedes           #+#    #+#             */
-/*   Updated: 2023/04/08 22:07:16 by gguedes          ###   ########.fr       */
+/*   Updated: 2023/04/08 22:21:09 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ int	mouse_hook(int x, int y, t_data *data)
 
 	(void)y;
 	player = &data->player;
-	mlx_mouse_move(data->mlx_ptr, data->win, round(WINDOW_WIDTH / 2), round(WINDOW_HEIGHT / 2));
-	if (x == 427)
-		player->turn_direction = 0;
-	else if (x > WINDOW_WIDTH / 2)
+	mlx_mouse_move(data->win, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+	// mlx_mouse_move(data->mlx_ptr, data->win, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+	if (x > WINDOW_WIDTH / 2 + 2)
 		player->turn_direction = 1;
-	else if (x < WINDOW_WIDTH / 2)
+	else if (x < WINDOW_WIDTH / 2 - 2)
 		player->turn_direction = -1;
+	else
+		player->turn_direction = 0;
 	return (0);
 }
